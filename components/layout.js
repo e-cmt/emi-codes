8de1e67f8108c6ac5ea1e4bc3c00556b10990737
Emi <emi@192.168.0.108>
2020-11-11 12:16:35 +0200

    Removed alert component

--- components/layout.js
+++ components/layout.js
@@ -10,17 +10,9 @@ export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        <link rel="icon" href="/favicon.png" />
+        <meta name="description" content="Emi's Portfolio - artist & coder" />
+        <meta property="og:image" content={`/favicon.png`} />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
