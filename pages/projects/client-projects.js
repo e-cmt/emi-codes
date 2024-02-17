fb17387b0907ce9c2174b0dccca3bba5fba33d24
e-cmt <71978113+e-cmt@users.noreply.github.com>
2024-02-17 13:45:23 +0200

    update content

--- pages/projects/client-projects.js
+++ pages/projects/client-projects.js
@@ -13,7 +13,7 @@ export default function ClientProjects() {
         title="NFT Marketplace"
         tags={['Photoshop', 'React', 'Next.JS', 'React Bootstrap', 'Wordpress']}
         date="sept 2021 - dec 2021"
-        body="Created an NFT marketplace where a user could buy, sell or exchange their collection of NFTs. The frontend was developed with React, Next.JS and React Bootstrap. Also developed a WordPress presentation website for the client."
+        body="Created an NFT marketplace where you could buy, sell or exchange your collection of NFTs. The frontend was developed with React, Next.JS and React Bootstrap. Also developed a WordPress presentation website for the client."
       />
 
       <Article
@@ -23,6 +23,7 @@ export default function ClientProjects() {
           'Figma',
           'React',
           'Next.JS',
+          'Vercel',
           'PostCSS',
           'npm',
           'Storybook',
@@ -32,14 +33,14 @@ export default function ClientProjects() {
           'Jira',
         ]}
         date="dec 2021 - apr 2023"
-        body="Worked on a custom library of components to be used for different brands. Created reusable components with React, integrated them with Sitecore and maintained conventions for uniformity. Built features in Storybook and unit tested them to prevent future issues. Worked asynchronously with a team of over 20 people."
+        body="Worked on a custom UI library of components to be used for different brands. Created reusable components with React, integrated them with Sitecore and maintained conventions for uniformity. Built features in Storybook and unit tested them to prevent future issues. Collaborated asynchronously with a cross-functional team to implement solutions that met business needs."
       />
 
       <Article
         title="Payments Service"
         tags={['Figma', 'React', 'Next.JS', 'CSS', 'Jira']}
         date="may 2023 - dec 2023"
-        body="Implemented payments into a website using a third-party service. The front-end was developed with React and deployed with Next.JS."
+        body="Implemented payments into a website using a third-party service. The frontend was developed with React and deployed with Vercel. Implemented unit testing to ensure the quality and reliability of the software."
       />
     </>
   );
