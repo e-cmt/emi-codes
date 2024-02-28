11f27fadd409e37c90eac97d8aaeadddb9408ddd
e-cmt <71978113+e-cmt@users.noreply.github.com>
2024-02-28 21:04:42 +0200

    update index

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -46,7 +46,7 @@ export default function Layout({ children }) {
           ref={dropdownRef}
           className={`${
             isActive ? 'active' : 'hidden'
-          } absolute w-full bg-white shadow-2xl delay-200 duration-200`}
+          } absolute w-full bg-white shadow-2xl delay-200 duration-200 z-10`}
         >
           <ul className="flex flex-col mt-4">
             <li>
